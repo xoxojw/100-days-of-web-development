cab80c03a4bb0bcc22d8cf092ac4fd0cb6c7a104
Jiwon Park <xoxojw.dev@gmail.com>
2024-01-11 17:27:10 +0900

    section 12 - 전역 window, document 객체, DOM 개념, DOM 트리 탐색

---
+++ app.js
@@ -0,0 +1,9 @@
+// console.log(document);
+// console.dir(document);
+// document.body.children[1].children[0].href = "https://google.com";
+
+let anchorElement = document.getElementById("external-link");
+anchorElement.href = "https://google.com";
+console.log(anchorElement.href);
+
+document.querySelector()
